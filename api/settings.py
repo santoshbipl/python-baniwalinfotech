10a2bec2ef1e8387711cc40226bbe5f133eb281f
santoshbipl <santosh@baniwalinfotech.com>
2024-12-27 16:44:47 +0530

    upload

--- api/settings.py
+++ api/settings.py
@@ -13,8 +13,6 @@ import os  # Add this line to import the os module
 
 from pathlib import Path
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 
