7b9d1cb445487f0edb3d65b3bf669d1340e12b58
santoshbipl <santosh@baniwalinfotech.com>
2024-12-30 10:34:04 +0530

    add

--- api/settings.py
+++ api/settings.py
@@ -51,8 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api.middleware.AddProgrammingLanguageHeaderMiddleware',
-
 ]
 
 ROOT_URLCONF = 'api.urls'
--- middleware.py
+++ middleware.py
@@ -1,11 +1,16 @@
-# middleware.py
+# myapp/middleware.py
 
-class AddProgrammingLanguageHeaderMiddleware:
+class CustomHeaderMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        # Process the request (optional)
+        
+        # Get the response
         response = self.get_response(request)
-        # Add a custom header to identify the backend language
-        response['X-Powered-By'] = 'Python/Django'
+        
+        # Add a custom header to the response
+        response['X-Custom-Header'] = 'Powered by Django'
+        
         return response
