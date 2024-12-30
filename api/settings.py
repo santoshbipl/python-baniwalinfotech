8e401bb9cc01fff5e3622424a4d81f4639992014
santoshbipl <santosh@baniwalinfotech.com>
2024-12-30 13:22:58 +0530

    test'

--- api/settings.py
+++ api/settings.py
@@ -26,6 +26,10 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# settings.py
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 #ALLOWED_HOSTS = []
